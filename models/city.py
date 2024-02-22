735643105490d6b615f8d7b070dbef4164e9109b
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-22 05:05:57 +0200

    edit file

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the city class"""
+"""This is the city module"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String

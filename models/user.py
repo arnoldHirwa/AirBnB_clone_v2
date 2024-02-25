859a344dc5e684bfd23312527c98a91209489e17
Hirwa Arnold <arnold.hirw@gmail.com>
2024-02-25 05:09:36 +0200

    new updates

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the user module"""
+"""This is the user class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -10,6 +10,11 @@ from models.review import Review
 
 class User(BaseModel, Base):
     """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
